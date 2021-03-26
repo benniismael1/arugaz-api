88ef790f2f2bbf9df60c87deed8d8482640b04af
bottes-squad-team <75607956+YakkoRasZ14@users.noreply.github.com>
2021-03-26 20:40:05 +0700

    Update textpro.js

--- src/textpro.js
+++ src/textpro.js
@@ -1,5 +1,5 @@
 const bent = require('bent')
-const baseURI = 'https://arugaz.my.id'
+const baseURI = 'https://ytbottes.herokuapp.com'
 
 module.exports.luxury = function luxury(query) {
 	return new Promise((resolve, reject) => {
